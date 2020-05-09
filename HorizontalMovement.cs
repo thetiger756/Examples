0365f77539f44f5f61484183120b7d9ebc59f1bc
thetiger756 <46494603+thetiger756@users.noreply.github.com>
2020-05-09 13:30:33 -0400

    Add files via upload

---
+++ HorizontalMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+//By Seth
+//This script is responsible for moving the player side-to-side.
+public class HorizontalMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 10f;
+
+    //I am choosing to use a CharacterController component, rather than a Rigidbody
+    //because it will be much more smooth since we do not need our player to be affected by physics.
+    //Think DOOM versus Marble Blast. We want DOOM.
+    //
+    //We need a reference to the character controller component.
+    private CharacterController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        HoriMovement();
+    }
+
+    private void HoriMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        //CharacterController.Move requires a Vector3.
+        Vector3 move = transform.right * x;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
---
+++ PlayerGravity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+//By Seth
+//This script is responsible for applying a downward force onto the player when in the air.
+public class PlayerGravity : MonoBehaviour
+{
+    [SerializeField]
+    private float gravity = -9.81f;
+
+    private Vector3 velocity;
+
+    private CharacterController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        velocity.y += gravity * Time.deltaTime;
+
+        //We multiply by time twice as this more closely simulates gravity.
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
